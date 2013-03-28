ca3499f03c1e113e91100dbb31695082f1e825c8
cole <cole.abramson@gmail.com>
2013-03-28 16:11:37 -0400

    fix event serializer

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,5 +1,5 @@
 class EventSerializer < ActiveModel::Serializer
-  attributes :id, :name, :dateof, :description, :url
+  attributes :id, :name, :when, :location, :description, :url
 
   def url
 	event_url(object)  	
